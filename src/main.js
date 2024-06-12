645ad754f433e9fabb84d9ec37d6d22e218b94a2
Andrew Ponomarev <spargo32@gmail.com>
2024-06-12 21:27:50 +0300

    Page loading spinner

--- src/main.js
+++ src/main.js
@@ -12,3 +12,11 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// Hide the page loading animation after the app is mounted
+window.addEventListener('load', () => {
+  const pageLoadingSpinner = document.getElementById('page-loading')
+  if (pageLoadingSpinner) {
+    pageLoadingSpinner.style.display = 'none'
+  }
+})
