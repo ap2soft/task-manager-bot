dca536e324095a6b326ef72f9cbc5f17991c14cf
Andrew Ponomarev <spargo32@gmail.com>
2024-06-14 23:31:25 +0300

    UserDetails: remove debug info

--- resources/js/Pages/TelegramWebApp/UserDetails.vue
+++ resources/js/Pages/TelegramWebApp/UserDetails.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
     <p class="text-sm text-[var(--tg-theme-hint-color)]">Hello @{{ userData.username ?? '%username%' }}.</p>
-    <div>
-      {{ tg.initDataUnsafe }}
-    </div>
-    <div>
-      {{ tg.initData }}
-    </div>
   </div>
 </template>
 
