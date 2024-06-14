ba7ca83f6a6777929a32a296ba101761ffb0338a
Andrew Ponomarev <spargo32@gmail.com>
2024-06-14 23:29:08 +0300

    Task list (front-end)

--- resources/js/Pages/TelegramWebApp/UserDetails.vue
+++ resources/js/Pages/TelegramWebApp/UserDetails.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <p class="text-sm text-[var(--tg-theme-hint-color)]">Hello @{{ userData.username ?? '%username%' }}.</p>
-    <code>{{ tg.initDataUnsafe }}</code>
-    <code>{{ tg.initData }}</code>
+    <div>
+      <code>{{ tg.initDataUnsafe }}</code>
+    </div>
+    <div>
+      <code>{{ tg.initData }}</code>
+    </div>
   </div>
 </template>
 
